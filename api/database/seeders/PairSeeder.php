582a0074ea86de5819130c9dcc8f06cce0661482
Khoa-Cifer <tdkhoa2409@gmail.com>
2024-12-20 17:28:08 +0700

    update seeder

--- api/database/seeders/PairSeeder.php
+++ api/database/seeders/PairSeeder.php
@@ -11,15 +11,15 @@ class PairSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     */
+    */
     public function run(): void
     {
         $pairs = [
             ['pair_symbol' => ToolsUtil::getPairSymbol('BTC', 'USD'), 'coin_symbol' => 'BTC', 'counter_symbol' => 'USD'],
             ['pair_symbol' => ToolsUtil::getPairSymbol('ETH', 'USD'), 'coin_symbol' => 'ETH', 'counter_symbol' => 'USD'],
             ['pair_symbol' => ToolsUtil::getPairSymbol('BNB', 'USD'), 'coin_symbol' => 'BNB', 'counter_symbol' => 'USD'],
-            ['pair_symbol' => ToolsUtil::getPairSymbol('XRP', 'USD'), 'coin_symbol' => 'XRP', 'counter_symbol' => 'USD'],
             ['pair_symbol' => ToolsUtil::getPairSymbol('SOL', 'USD'), 'coin_symbol' => 'SOL', 'counter_symbol' => 'USD'],
+            ['pair_symbol' => ToolsUtil::getPairSymbol('XRP', 'USD'), 'coin_symbol' => 'XRP', 'counter_symbol' => 'USD'],
             ['pair_symbol' => ToolsUtil::getPairSymbol('UNI', 'USD'), 'coin_symbol' => 'UNI', 'counter_symbol' => 'USD'],
             ['pair_symbol' => ToolsUtil::getPairSymbol('LINK', 'USD'), 'coin_symbol' => 'LINK', 'counter_symbol' => 'USD'],
         ];
