394e7c5adfe7ae3eb22611132a9b5895d3920193
Khoa-Cifer <tdkhoa2409@gmail.com>
2024-12-22 21:55:24 +0700

    fix cors

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://bot.onexmm.com', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
