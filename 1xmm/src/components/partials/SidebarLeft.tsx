44a9b597633558a6a85ff25faca2e95af9cc163e
Khoa-Cifer <tdkhoa2409@gmail.com>
2024-12-22 21:15:07 +0700

    update migration

--- 1xmm/src/components/partials/SidebarLeft.tsx
+++ 1xmm/src/components/partials/SidebarLeft.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { useTonConnectUI } from "@tonconnect/ui-react";
 import DetailStar from "./components/Star/DetailStar";
-import { toast } from "react-toastify";
 import { userProfileStore } from "@/store/user-store";
 
 interface SidebarProps {
