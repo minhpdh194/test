25aa516bca7e71e3e3dd3998da05e08d189cfc42
Trần Đăng Khoa <tdkhoa2409@gmail.com>
2025-02-27 23:32:31 +0700

    n

--- 1xmm/src/pages/Earn.tsx
+++ 1xmm/src/pages/Earn.tsx
@@ -21,29 +21,8 @@ export default function Profile() {
       <Header amount_token={userProfile.amount_of_tokens} />
       <div className="mt-4 mb-8 text-center">
         <span className="fw-bold text-2xl">{t(`${earn}.earn_more_coins`)}</span>
-        <img
-          src="/images/earn/shilling.png"
-          alt="money"
-          className="w-20 h-24 absolute top-35 transform -translate-x-1/2"
-          style={{
-            left: `15%`
-          }}
-        />
-        <div className="bg-[linear-gradient(180deg,#F79841_38.34%,#F9D838_100%)] p-2 rounded-xl mt-3" style={{ float: `inline-end`, width: `90%` }}>
-          <div className="flex justify-between p-1">
-            <span className="text-end fw-bold">
-            </span>
-            <span className="text-end fw-bold text-lg">
-              {t(`${earn}.daily_rewards`)}
-            </span>
-            <span className="text-end">
-              <span className="fw-bold">10</span>h :  <span className="fw-bold">10</span>m : <span className="fw-bold">10</span>s
-            </span>
-          </div>
-          <span className="text-center text-sm">{t(`${earn}.hurry_up_and_get_it`)}</span>
-        </div>
       </div>
-      <div className="mt-28 mb-8">
+      <div className="mb-8">
         <div className="flex justify-between items-center">
           <div className="fw-bold">
             {t(`${earn}.daily_quests`)}
