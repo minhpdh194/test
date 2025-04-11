f53121bbd549eb73f0282cfec5db5c84ff31d593
Trần Đăng Khoa <tdkhoa2409@gmail.com>
2025-04-11 11:19:16 +0700

    fix error

--- api/app/Models/TelegramUser.php
+++ api/app/Models/TelegramUser.php
@@ -61,29 +61,31 @@ class TelegramUser extends Authenticatable
         $this->save();
     }
 
-    public static function tap($count = 1, $updateTime, $gameData, $earnPerTap)
+    public function tap($count = 1, $earnPerTap)
     {
-        $gameData->available_energy += TelegramUser::restoreEnergy($updateTime, $gameData->energy_limit, $gameData->last_tap);
-        if ($gameData->available_energy > $gameData->energy_limit) $gameData->available_energy = $gameData->energy_limit;
+        $gameData = UserGameData::where('telegram_user_id', $this->telegram_user_id)->first();
+
+        $available_energy = $gameData->available_energy + $this->restoreEnergy($gameData->energy_limit, $this->last_login);
+        if ($available_energy > $gameData->energy_limit) $available_energy = $gameData->energy_limit;
 
         $totalEnergyRequired = $count * $earnPerTap;
 
         // We allow only to tap until remaining energy is empty
-        if ($gameData->available_energy < $totalEnergyRequired) {
-            $totalEnergyRequired = $gameData->available_energy;
-            $count = floor($totalEnergyRequired / $earnPerTap);
-            $totalEnergyRequired = $count * $earnPerTap;
+        if ($available_energy < $totalEnergyRequired) {
+            $totalEnergyRequired = $available_energy;
+            $count = $totalEnergyRequired / $earnPerTap;
         }
 
+        // $multiplier = $this->getActiveBoosterMultiplier();
         $multiplier = 1;
+
         $earned = $totalEnergyRequired * $multiplier;
 
         $gameData->balance += $earned;
         $gameData->amount_of_tokens += $earned;
         $gameData->available_energy -= $totalEnergyRequired;
-        $gameData->last_tap = $updateTime;
-        $gameData->save();
 
+        $gameData->save();
         return [
             'earned' => $earned,
             'balance' => $gameData->balance,
@@ -92,10 +94,10 @@ class TelegramUser extends Authenticatable
         ];
     }
 
-    public static function restoreEnergy($update_time, $maxEnergy, $last_tap)
+    public function restoreEnergy($maxEnergy, $last_login)
     {
-        $freq = abs($update_time->floatDiffInMinutes(Carbon::parse($last_tap)));
-        if ($freq > 180.0) $freq = 180.0;
-        return ceil($freq / 180.0 * $maxEnergy);
+        $freq = Carbon::parse($last_login)->diffInHours(Carbon::now());
+        if ($freq > 3) $freq = 3;
+        return floor($freq / 3 * $maxEnergy);
     }
 }
